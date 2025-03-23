e53550918aed08a86c7178f066caab2489875fa0
clawzz22 <fachrurizalaryandi@gmail.com>
2025-03-23 16:37:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,5 @@ const CONFIG = {
     mess: 'makasi loh sumpah ahahahahahahas',
     messDesc: 'beneran makasih',
     btnAccept: 'Okeeee'
+    messLink: 'https://tenor.com/view/mark-lee-nct127-nct-yeah-gif-19984969'
 }

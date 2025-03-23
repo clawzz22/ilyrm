5b54ad174632abe98600afc5f84786e01587fb7c
clawzz22 <fachrurizalaryandi@gmail.com>
2025-03-23 16:47:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     titleWeb: "ily mabar yuuk",
     introTitle: "haloo ily orang terkeren se dunia",
     introDesc: "kamuuu lagii apaaa, dc yuuk kita mabar",
-    btnIntro: "HEHEHEH",
+    btnIntro: "LANJUT SINIII",
     title: "ILY ORANG KEREN AYO MABAR JANGAN SIBUK MULU (AKU SIH YG SIBUK)",
     desc: "BTW IL AKU GANTENG GA, JUJUR HAYO",
     btnYes: "GANTENG BANGET",

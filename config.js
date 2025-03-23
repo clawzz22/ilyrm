a79f5be77670a9971fa28111cdd3cfad318f7b7e
clawzz22 <fachrurizalaryandi@gmail.com>
2025-03-23 16:38:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "ily mabar yuuk",
-    introTitle: 'haloo ily orang terkeren se dunia',
-    introDesc: `kamuuu lagii apaaa, dc yuuk kita mabar`,
-    btnIntro: 'HEHEHEH',
-    title: 'ILY ORANG KEREN AYO MABAR JANGAN SIBUK MULU (AKU SIH YG SIBUK)',
-    desc: 'BTW IL AKU GANTENG GA, JUJUR HAYO',
-    btnYes: 'GANTENG BANGET',
-    btnNo: 'NggaK',
-    question: 'mark lee sama aku ganteng mana ?',
-    btnReply: 'Send',
-    reply: 'aryandi lah dia kan lebih ganteng dari flavagen juga',
-    mess: 'makasi loh sumpah ahahahahahahas',
-    messDesc: 'beneran makasih',
-    btnAccept: 'Okeeee'
-    messLink: 'https://tenor.com/view/mark-lee-nct127-nct-yeah-gif-19984969'
-}
+    introTitle: "haloo ily orang terkeren se dunia",
+    introDesc: "kamuuu lagii apaaa, dc yuuk kita mabar",
+    btnIntro: "HEHEHEH",
+    title: "ILY ORANG KEREN AYO MABAR JANGAN SIBUK MULU (AKU SIH YG SIBUK)",
+    desc: "BTW IL AKU GANTENG GA, JUJUR HAYO",
+    btnYes: "GANTENG BANGET",
+    btnNo: "NggaK",
+    question: "mark lee sama aku ganteng mana ?",
+    btnReply: "Send",
+    reply: "aryandi lah dia kan lebih ganteng dari flavagen juga",
+    mess: "makasi loh sumpah ahahahahahahas",
+    messDesc: "beneran makasih",
+    btnAccept: "Okeeee",
+    messLink: "https://tenor.com/view/mark-lee-nct127-nct-yeah-gif-19984969"
+};

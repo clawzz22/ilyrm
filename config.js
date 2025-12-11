1cbdf49842be062f4532109722b4a9e9d307074a
clawzz22 <fachrurizalaryandi@gmail.com>
2025-12-12 00:04:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "ily mabar yuuk",
-    introTitle: "haloo ily orang terkeren se dunia",
-    introDesc: "kamuuu lagii apaaa, dc yuuk kita mabar",
+    titleWeb: "bibuu kesayangannya akuuu",
+    introTitle: "haloo bibuuu orang terkeren se dunia",
+    introDesc: "kamuuu lagii apaaan hehhehehehehehehehe",
     btnIntro: "LANJUT SINIII",
-    title: "ILY ORANG KEREN AYO MABAR JANGAN SIBUK MULU (AKU SIH YG SIBUK)",
-    desc: "BTW IL AKU GANTENG GA, JUJUR HAYO",
+    title: "bibuuuu nantiii sabtuuu maiin kesinii yaaa kitaaa heaaaliiinggg heehehehehehehehehehehehehehehehhehehe)",
+    desc: "BTW BEEEEEE AKU GANTENG GA, JUJUR HAYO",
     btnYes: "GANTENG BANGET",
     btnNo: "NGGAK",
-    question: "mark lee sama aku ganteng mana ?",
+    question: "Rio Haryanto sama aku ganteng mana ?",
     btnReply: "Send",
-    reply: "aryandi lah dia kan lebih ganteng dari flavagen juga",
-    mess: "makasi loh sumpah ahahahahahahas",
+    reply: "DWI LISTIANI CEWENYA ARYANDI MENGAKUI KALO ARYANDI ADALAH ORANG TERGANTENG SE-ALAM SEMESTA",
+    mess: "makasi loh beee ga nyangka km bakal jawab kaya giniii",
     messDesc: "beneran makasih",
     btnAccept: "Okeeee",
-    messLink: "https://www.roblox.com/"
+    messLink: "https://open.spotify.com/track/4B3DEANIvod8552CbwftCG?si=e8c62216c3124c1c"
 };
